93527b2892da19e2f9cf16227eaf0bf02bcbe816
Leyder Quintero <lele.quinter@gmail.com>
2022-09-14 10:06:58 -0500

    One way data binding

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -5,5 +5,23 @@ import { Component } from "@angular/core";
   templateUrl: './heroe.component.html',
 })
 export class HeroeComponent{
+nombre: string = 'Ironman'
+edad: number = 45
+
+get nombreCapitalizado():string{
+  return this.nombre.toUpperCase();
+}
+
+obtenerNombre(): string{
+  return ` ${this.nombre} - ${this.edad}`
+}
+
+cambiarNombre():void{
+  this.nombre = 'Spider-man';
+}
+
+cambiarEdad():void{
+  this.edad = 16;
+}
 
 }
