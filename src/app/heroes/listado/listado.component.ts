9b815d31c4f6acfe4fa38ece044c30aafde94d5d
Leyder Quintero <lele.quinter@gmail.com>
2022-09-14 15:25:26 -0500

    Directiva ngFor

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent implements OnInit {
+export class ListadoComponent {
 
-  constructor() { }
+heroes: string[] = ['spiderman', 'ironman', '3Dman', 'Wolverine']
+heroeBorrado: string = '';
 
-  ngOnInit(): void {
-  }
+borrarHeroe(){
+   this.heroeBorrado = this.heroes.pop() || '';
+}
 
 }
