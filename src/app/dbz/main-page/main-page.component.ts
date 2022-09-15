94e69819dbc01d14c063828f3e386a2b9ad8013d
Leyder Quintero <lele.quinter@gmail.com>
2022-09-15 15:00:00 -0500

    Mostrar listado de personajes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
   }
 
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegetta',
+      poder: 12000
+    }
+  ]
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+    // console.log(this.nuevo);
+
+    this.personajes.push(this.nuevo);
+    this.clear();
+    // event.preventDefault();
+  }
+
+  clear(): void {
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+  }
 }
