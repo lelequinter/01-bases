f32347d3bfa26d47de2bd4ed15c7a2e3378cc5b7
Leyder Quintero <lele.quinter@gmail.com>
2022-09-15 15:11:30 -0500

    crear componentes hijos

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -31,14 +31,16 @@ export class MainPageComponent {
     if (this.nuevo.nombre.trim().length === 0) {
       return;
     }
-    // console.log(this.nuevo);
 
     this.personajes.push(this.nuevo);
+    console.log(this.nuevo);
     this.clear();
     // event.preventDefault();
   }
 
   clear(): void {
+    // this.nuevo.nombre = ''
+    // this.nuevo.poder = 0
     this.nuevo = {
       nombre: '',
       poder: 0
